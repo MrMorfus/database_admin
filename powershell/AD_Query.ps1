2b9c875bc747b8c61bc6ddd8bb118f4d55dbea77
MrMorfus <123595725+MrMorfus@users.noreply.github.com>
2023-03-13 15:54:13 -0600

    Add files via upload

---
+++ powershell/AD_Query.ps1
@@ -0,0 +1,13 @@
+﻿$GFile = New-Item -type file -force "C:\Scripts\GroupDetails.csv" 
+Import-CSV "C:\Scripts\GList.csv" | ForEach-Object { 
+$GName = $_.GroupName 
+$group = [ADSI] "LDAP://$GName" 
+$group.cn 
+$group.cn | Out-File $GFile -encoding ASCII -append 
+    foreach ($member in $group.member) 
+        { 
+            $Uname = new-object directoryservices.directoryentry("LDAP://$member") 
+            $Uname.cn 
+            $Uname.cn | Out-File $GFile -encoding ASCII -append 
+        } 
+}
